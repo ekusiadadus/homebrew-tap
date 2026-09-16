5c4e0d5dc1f15178424f16c0b08b2fa192faaace
ekusiadadus <ekusiadadus@gmail.com>
2026-09-16 10:39:34 +0900

    chore: update ttybird to 0.10.2

--- Formula/ttybird.rb
+++ Formula/ttybird.rb
@@ -1,22 +1,22 @@
 class Ttybird < Formula
   desc "Find coding agents and return to their terminals"
   homepage "https://github.com/ekusiadadus/ttybird"
-  version "0.9.0"
+  version "0.10.2"
   license "MIT"
 
   on_macos do
     depends_on arch: :arm64
     depends_on macos: :sonoma
 
-    url "https://github.com/ekusiadadus/ttybird/releases/download/v0.9.0/ttybird-0.9.0-aarch64-apple-darwin.tar.gz"
-    sha256 "257aa983aa232c81b61bdd5c3a1f2d542b0ce5f1c967b72b8079e203c7d62bcb"
+    url "https://github.com/ekusiadadus/ttybird/releases/download/v0.10.2/ttybird-0.10.2-aarch64-apple-darwin.tar.gz"
+    sha256 "851ce66e2302a9e431e2ec2b3e53a073f603870551cd6a270eeccff262bec579"
   end
 
   on_linux do
     depends_on arch: :x86_64
 
-    url "https://github.com/ekusiadadus/ttybird/releases/download/v0.9.0/ttybird-0.9.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "8104a929bd097d49c16f41909ad113f64a76373d65bae9463f8858fb3e11aeb4"
+    url "https://github.com/ekusiadadus/ttybird/releases/download/v0.10.2/ttybird-0.10.2-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "83d29a3e052aa1dea9193c53d3f309e1ac47461a925dafc470c4fbdcfc42ea6f"
   end
 
   def install
